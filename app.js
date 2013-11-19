d83a27a6a4dca916f77120618282d81585930be4
Andrew Ho <aho@konradgroup.com>
2013-11-19 16:13:33 +0000

    first commit

--- app.js
+++ app.js
@@ -7,17 +7,20 @@ var express = require('express'),
   routes = require('./routes'),
   api = require('./routes/api'),
   http = require('http'),
-  path = require('path');
-
+  path = require('path'),
+  qs = require('querystring');
 
 var app = module.exports = express();
+var server = http.createServer(app);
+var io = io.listen(server);
+
 
 /**
 * Configuration
 */
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 80);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.logger('dev'));
@@ -44,14 +47,24 @@ app.get('/partial/:name', routes.partial);
 
 // JSON API
 app.get('/api/name', api.name);
+app.post('/sensor', function(req, res) {
+    var body = '';
+    req.on('data', function(data) {
+       body += data;
+    });
+    req.on('end', function() {
+       var POST = qs.parse(body);
+       console.log(POST);
+       res.end();
+    });
+});
 
 // redirect all others to the index (HTML5 history)
 app.get('*', routes.index);
+
 
 /**
 * Start Server
 */
 
-http.createServer(app).listen(app.get('port'), function () {
-  console.log('Express server listening on port ' + app.get('port'));
-});
+server.listen(80);
